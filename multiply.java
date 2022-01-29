3b41cac02e38c2f9b09c4702da090e68ff91c870
Tejaswini <ktejaswini015@gmail.com>
2022-01-29 11:32:28 +0530

    added multiply.java

---
+++ multiply.java
@@ -0,0 +1,6 @@
+public class multiply{
+    public static void main(String[] args){
+
+        System.out.println("a * b " + 5 * 6 );
+    }
+}
--- s.java
+++ s.java
@@ -1,6 +1,6 @@
 public class s{
     public static void main(String[] args){
 
-        System.out.println(5 + 6 );
+        System.out.println("a + b " + 5 + 6 );
     }
 }
